1c44b04259171e917a98a099ac869dd5b0c673a3
victorniubo <victorniubo3@gmail.com>
2021-12-03 21:06:58 -0300

    agora pronto :P

--- main.py
+++ main.py
@@ -14,12 +14,13 @@ key_name = client_django.createKeyPair("django_key_v")
 client_django.createSecurityGroup("Django_SG", "Security Group do Django", [22, 80, 8080])
 djangoIp, djangoId = client_django.createInstance(name="Django_v", initial_script=open("django.sh", "r").read().replace("postgresIp", postgresIp), sg="Django_SG", key='django_key_v')
 img_id = client_django.createIMG('DjangoIMG', "Imagem de instancia com Django instalado", djangoId)
-client_django.killAll()
+client_django.killDjango()
+
 tg_arn = client_django.createTargetGroup('targetgroupV')
 client_django.deleteLoadBalancer('loadbalancerV')
-
 sg_id = client_django.createSecurityGroup('LoadBalancer_SG', "Security Group do Load Balancer", [22, 80, 8080])
 lb_arn = client_django.createLoadBalancer('loadbalancerV', sg_id)
+
 client_django.createListener(lb_arn, tg_arn)
 client_django.createSecurityGroup('AutoScaling_SG', 'Security Group do AutoScaler', [80, 8080])
 client_django.createAutoScaling('autoscalingV', 'launchconfigV', img_id, key_name, 'AutoScaling_SG', tg_arn)
